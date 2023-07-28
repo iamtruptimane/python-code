12fb4697a000ca6ba51577176bbaad8a75a9a817
Your Name <you@example.com>
2023-07-28 11:32:46 +0530

    initial commit

---
+++ day2/demo1.py
@@ -0,0 +1,22 @@
+#int
+num = 100
+print(f"number={num}, data type ={type(num)}")
+
+#float
+salary=12.5
+print(f"salary={salary}, data type={type(salary)}")
+
+#string (single line value)
+first_name = "Trupti"
+print(f"first name= {first_name}, data type={type(first_name)}")
+
+#string (multiple line)
+address = """2242, kachare galli,
+urun islampur,
+maharashtra,
+India"""
+print(f"address={address}, data type={type(address)}")
+
+#bool
+candate = True
+print(f"can date ={candate}, data type={type(candate)}")
