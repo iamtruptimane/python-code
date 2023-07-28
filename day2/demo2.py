9a0697666ea805b3681385b3838925d5d3e254b8
Your Name <you@example.com>
2023-07-28 11:48:06 +0530

    2nd file added

---
+++ day2/demo2.py
@@ -0,0 +1,37 @@
+n1 = 20
+n2 = 30
+
+#addition
+addition = n1 + n2
+print(f"{n1} + {n2} = {addition}")
+
+#substraction
+subtraction = n1 - n2
+print(f"{n1} - {n2}= {subtraction}")
+
+#multiplication
+multiplication = n1 * n2
+print(f"{n1} * {n2} = {multiplication}")
+
+#division (true)
+division = n1 / n2
+print(f"{n1} / {n2} = {division}")
+
+#division (floor)
+division = n1 // n2
+print(f"{n1} / {n2} = {division}")
+
+#20 th to the power 2
+power= n2 ** 2
+print(f"power of {n2} = {power}")
+
+#reminder (mod)
+mod= 11 % 2
+print(f"mod= {mod}")
+
+#string * operator
+print(f"-" * 5)
+
+#string + op (cancatenation)
+print(f"hello" + "world")
+
